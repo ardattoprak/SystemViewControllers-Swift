8b00c24de1de3c3f1371c1ab7dab9be0b3c9cad3
ardatoprak <ardattoprak@gmail.com>
2023-01-17 20:57:30 +0300

    UI has been updated and shareButtonTapped function added

--- System View Controllers/ViewController.swift
+++ System View Controllers/ViewController.swift
@@ -9,10 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - UI Elements
+    @IBOutlet var imageView: UIImageView!
+    
+    
+    //MARK: - Properties
+    
+    //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //MARK: - Functions
+    
+    //MARK: - Actions
+    @IBAction func shareButtonTapped(_ button: UIButton){
+        // image var mı emin oluyoruz.
+        guard let image = imageView.image else { return }
+        
+        // Adım 1 : ActivityController nesnesi oluştur.
+        
+        // activityItems: paylaşılacak olan nesneler. (String, Image, Int, URL, vs..)
+        // applicationActivities: Paylaşılacak olan nesnelerin hangi platformlarda paylaşılmasına izin verileceği. Örn: (Twitter, Messages, Email)
+        let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        
+        
+        // Bulunduğunuz Controller'da yeni bir Controller göstermek için kullanılır.
+        
+        present(activityController, animated: true)
+        
+        
+    }
+    
+    @IBAction func safariButtonTapped(_ button: UIButton){
+        
+    }
+    
+    @IBAction func photosButtonTapped(_ button: UIButton){
+        
+    }
+    
+    @IBAction func emailButtonTapped(_ button: UIButton){
+        
+    }
+
+
 
 
 }
